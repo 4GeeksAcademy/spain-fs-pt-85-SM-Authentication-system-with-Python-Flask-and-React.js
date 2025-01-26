ebbb538fccbb1bc519f2e6fb21a544443f5bd869
unduashe <unduashe@outlook.es>
2025-01-26 18:51:01 +0000

    tablas, relaciones y endpoints para hacer GET y POST de usuarios y favoritos agregados

--- src/app.py
+++ src/app.py
@@ -5,10 +5,12 @@ import os
 from flask import Flask, request, jsonify, url_for
 from flask_migrate import Migrate
 from flask_swagger import swagger
+from sqlalchemy import select
+from sqlalchemy.orm.exc import NoResultFound, MultipleResultsFound
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, User, Favourites, People, Planets
 #from models import Person
 
 app = Flask(__name__)
@@ -36,13 +38,114 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
+# enpoints de user
 @app.route('/user', methods=['GET'])
-def handle_hello():
-
+def get_users():
+    data = db.session.scalars(select(User)).all()
+    results = list(map(lambda item: item.serialize(), data))
+    if results == []:
+        results = "there aren't any users in the database"
     response_body = {
-        "msg": "Hello, this is your GET /user response "
+        "result": results
     }
+    return jsonify(response_body), 200
 
+# enpoints de favourites
+@app.route('/users/favorites', methods=['GET'])
+def get_favourites():
+    data = db.session.scalars(select(Favourites)).all()
+    results = list(map(lambda favourite: favourite.serialize(), data))
+    if results == []:
+        results = "there aren't any favourites"
+    response_body = {
+        "results": results
+    }
+    return jsonify(response_body), 200
+
+@app.route('/favorite/planet/<int:planet_id>', methods=['POST'])
+def add_favourite_planet(planet_id):
+    request_data = request.json
+    user_id = request_data["user_id"]
+    try:
+        planet = db.session.execute(db.select(Planets).filter_by(id=planet_id)).scalar_one()
+    except NoResultFound:
+        return jsonify({"error": "planet not found."}), 404
+    try:
+        user = db.session.execute(db.select(User).filter_by(id=user_id)).scalar_one()
+    except NoResultFound:
+        return jsonify({"error": "user not found"}), 404
+    new_favourite = Favourites(
+        users_favourites = user,
+        planet_favourites = planet
+    )
+    db.session.add(new_favourite)
+    db.session.commit()
+    return jsonify(new_favourite.serialize()), 200
+
+@app.route('/favorite/people/<int:people_id>', methods=['POST'])
+def add_favourite_person(people_id):
+    request_data = request.json
+    user_id = request_data["user_id"]
+    if "user_id" not in request_data:
+        return jsonify({"error": "key user_id is obligatory."}), 400
+    try:
+        person = db.session.execute(db.select(People).filter_by(id=people_id)).scalar_one()
+    except NoResultFound:
+        return jsonify({"error": "person not found."}), 404
+    try:
+        user = db.session.execute(db.select(User).filter_by(id=user_id)).scalar_one()
+    except NoResultFound:
+        return jsonify({"error": "user not found"}), 404
+    new_favourite = Favourites(
+        users_favourites = user,
+        people_favourites = person
+    )
+    db.session.add(new_favourite)
+    db.session.commit()
+    return jsonify(new_favourite.serialize()), 200
+
+# enpoints de people
+@app.route('/people', methods=['GET'])
+def get_people():
+    data = db.session.scalars(select(People)).all()
+    print(data)
+    results = list(map(lambda person: person.serialize(), data))
+    response_body = {
+        "results": results
+    }
+    return jsonify(response_body), 200
+
+
+@app.route('/people/<int:people_id>', methods=['GET'])
+def get_specific_users(people_id):
+    try:
+        person = db.session.execute(select(People).filter_by(id=people_id)).scalar_one()
+    except NoResultFound:
+        return jsonify({"error": "Person not found."}), 404
+    result_body = {
+        "result": person.serialize()
+    }
+    return jsonify(result_body), 200
+
+# enpoints de planets
+@app.route('/planets', methods=['GET'])
+def get_planets():
+    data = db.session.scalars(select(Planets)).all()
+    results = list(map(lambda planet: planet.serialize(), data))
+    result_body = {
+        "results": results
+    }
+    return jsonify(result_body), 200
+
+@app.route('/planets/<int:planet_id>', methods=['GET'])
+def get_specific_planet(planet_id):
+    try:
+        planet = db.session.execute(db.select(Planets).filter_by(id=planet_id)).scalar_one()
+    except NoResultFound:
+        return jsonify({"error": "Planet not found."}), 404
+    response_body = {
+        "results": planet.serialize()
+    }
     return jsonify(response_body), 200
 
 # this only runs if `$ python src/app.py` is executed
--- src/models.py
+++ src/models.py
@@ -1,6 +1,7 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy.orm import mapped_column
-from sqlalchemy import Integer, String, Boolean
+from sqlalchemy.orm import mapped_column, Mapped, relationship
+from sqlalchemy import ForeignKey, Integer, String, Float
+from typing import List
 
 db = SQLAlchemy()
 
@@ -8,22 +9,128 @@ db = SQLAlchemy()
 class User(db.Model):
     __tablename__ = "user"
 
-    id = mapped_column(Integer, primary_key=True)
-    email = mapped_column(String(120), nullable=False)
-    password = mapped_column(String(80))
-    is_active = mapped_column(Boolean)
+    id: Mapped[int] = mapped_column(primary_key=True)
+    email: Mapped[str] = mapped_column(String(120), nullable=False)
+    password: Mapped[str] = mapped_column(String(80), nullable=False)
+    favourites_users: Mapped[List["Favourites"]] = relationship(back_populates="users_favourites")
 
     def __repr__(self):
-        return '<User %r>' % self.username
+        return '<User %r>' % self.email
 
     def serialize(self):
         return {
             "id": self.id,
             "email": self.email,
+            "favourites": self.favourites_users
             # do not serialize the password, its a security breach
         }
 
+class Favourites(db.Model):
+    __tablename__= "favourites"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    # relación con usuarios
+    users_favourites_id: Mapped[int] = mapped_column(ForeignKey("user.id"))
+    users_favourites: Mapped["User"] = relationship(back_populates="favourites_users")
+    # relación con people
+    people_favourites_id: Mapped[int] = mapped_column(ForeignKey("people.id"), nullable=True)
+    people_favourites: Mapped["People"] = relationship()
+    # relación con planets
+    planet_favourites_id: Mapped[int] = mapped_column(ForeignKey("planets.id"), nullable=True)
+    planet_favourites: Mapped["Planets"] = relationship()
 
+    def serialize(self):
+        return {
+            "id": self.id,
+            "user_id": self.users_favourites_id,
+            "people": self.people_favourites.serialize() if self.people_favourites else None,
+            "planets": self.planet_favourites.serialize() if self.planet_favourites else None
+        }
+    
+
+class People(db.Model):
+    __tablename__= "people"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(250), nullable=False)
+    birth_year: Mapped[int] = mapped_column(Integer, nullable=True)
+    eye_color: Mapped[str] = mapped_column(String(50), nullable=True)
+    gender: Mapped[str] = mapped_column(String(50), nullable=True)
+    hair_color: Mapped[str] = mapped_column(String(50), nullable=True)
+    height: Mapped[str] = mapped_column(String(50), nullable=True)
+    weight: Mapped[str] = mapped_column(String(50), nullable=True)
+    skin_color: Mapped[str] = mapped_column(String(50), nullable=True)
+    species: Mapped[str] = mapped_column(String(50), nullable=True)
+    starships: Mapped[str] = mapped_column(String(250), nullable=True)
+    vehicles: Mapped[str] = mapped_column(String(250), nullable=True)
+    master: Mapped[str] = mapped_column(String(250), nullable=True)
+    disciple: Mapped[str] = mapped_column(String(250), nullable=True)
+    image: Mapped[str] = mapped_column(String, nullable=True)
+    films: Mapped[str] = mapped_column(String, nullable=True)
+    # relación con favoritos
+    # favourites_people: Mapped[List["Favourites"]] = relationship(back_populates="people_favourites")
+    # relación con planets
+    homeworld_id: Mapped[int] = mapped_column(ForeignKey("planets.id"), nullable=True)
+    homeworld: Mapped[List["Planets"]] = relationship(back_populates="resident")
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "birth_year": self.birth_year,
+            "eye_color": self.eye_color,
+            "gender": self.gender,
+            "hair_color": self.hair_color,
+            "height": self.height,
+            "weight": self.weight,
+            "skin_color": self.skin_color,
+            "species": self.species,
+            "starships": self.starships,
+            "vehicles": self.vehicles,
+            "master": self.master,
+            "disciple": self.disciple,
+            "image": self.image,
+            "films": self.films,
+            # "favourites": self.favourites_people,
+            "homeworld": self.homeworld.serialize() if self.homeworld else None
+        }
+
+class Planets(db.Model):
+    __tablename__= "planets"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(250), nullable=False)
+    diameter: Mapped[str] = mapped_column(String(50), nullable=True)
+    rotation_period: Mapped[str] = mapped_column(String(50), nullable=True)
+    orbital_period: Mapped[str] = mapped_column(String(50), nullable=True)
+    gravity: Mapped[float] = mapped_column(Float, nullable=True)
+    population: Mapped[int] = mapped_column(nullable=True)
+    climate: Mapped[str] = mapped_column(String(50), nullable=True)
+    terrain: Mapped[str] = mapped_column(String(50), nullable=True)
+    surface_water: Mapped[str] = mapped_column(String(50), nullable=True)
+    image: Mapped[str] = mapped_column(String, nullable=True)
+    species: Mapped[str] = mapped_column(String(100), nullable=True)
+    films: Mapped[str] = mapped_column(String(500), nullable=True)
+    # relación con favoritos
+    # favourites_planet: Mapped[List["Favourites"]] = relationship(back_populates="planet_favourites")
+    # relación con people
+    resident: Mapped[List["People"]] = relationship(back_populates="homeworld")
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "diameter": self.diameter,
+            "rotation_period": self.rotation_period,
+            "orbital_period": self.orbital_period,
+            "gravity": self.gravity,
+            "population": self.population,
+            "climate": self.climate,
+            "terrain": self.terrain,
+            "surface_water": self.surface_water,
+            "image": self.image,
+            "species": self.species,
+            "films": self.films,
+            # "favourites": self.favourites_planet,
+            "resident": [person.serialize for person in self.resident]
+        }
 
 # from flask_sqlalchemy import SQLAlchemy
 
